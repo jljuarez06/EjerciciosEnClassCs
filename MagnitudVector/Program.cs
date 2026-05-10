f3881433ef78b24f5177c0cef5c010a9e90288f5
Jorge <jljuarez@uamv.edu.ni>
2026-05-10 10:04:10 -0600

    Add files via upload

---
+++ MagnitudVector/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] vector1 = new int [5];
+double sumaCuadrados = 0;
+
+Console.WriteLine("Ingrese los elementos del vector:");
+for (int i = 0; i < vector1.Length; i++)
+{
+    try
+    {
+        Console.WriteLine($"Elemento {i + 1}:");
+        vector1[i] = int.Parse(Console.ReadLine()!);
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Error: Ingrese un número entero válido.");
+        i--; 
+    }
+}
+for (int i = 0; i < vector1.Length; i++)
+{
+    sumaCuadrados += Math.Pow(vector1[i], 2);
+}
+double magnitud = Math.Sqrt(sumaCuadrados);
+Console.WriteLine("La magnitud del vector es: " + magnitud);
